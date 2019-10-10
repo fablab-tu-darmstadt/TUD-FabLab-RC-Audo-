9145d33e806dffc5fb3ec4fc170e95b60000ea12
NilsWittig <nilswittig96@gmail.com>
2019-10-10 19:46:56 +0200

    Setup communication with STM32

--- CubeMX/Src/comm.c
+++ CubeMX/Src/comm.c
@@ -2,8 +2,18 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "stm32f3xx_hal.h"
+#include <stdio.h>
 
 #define MAX_MSG_LENGTH 100
+#define MAXSPLITS 10
+
+typedef struct SplittedStr_
+{
+    uint8_t cnt;
+    char* strs[MAXSPLITS];
+    bool completelySplitted;
+} SplittedStr_t;
+
 
 UART_HandleTypeDef huart2;
 char msg[MAX_MSG_LENGTH];
@@ -12,6 +22,52 @@ uint8_t index;
 void comm_init()
 {
 	index = 0;
+}
+
+void strsplit(SplittedStr_t* sstr,
+                    char* str,
+                    char splitchar,
+                    char quotechar,
+                    uint8_t maxsplits);
+
+void parse()
+{
+
+	SplittedStr_t sstr;
+	strsplit(&sstr, msg, ' ', '"', 10);
+
+	char send[100];
+
+	/*
+	sprintf(send, "Splits = %i\n",sstr.cnt);
+
+	HAL_UART_Transmit(&huart2, (uint8_t*)send, strlen(send), 0xFFFF);
+
+
+
+	HAL_UART_Transmit(&huart2, (uint8_t*)sstr.strs[0], strlen(sstr.strs[0]), 0xFFFF);
+
+	sprintf(send, "\n");
+	HAL_UART_Transmit(&huart2, (uint8_t*)send, strlen(send), 0xFFFF);
+
+	HAL_UART_Transmit(&huart2, (uint8_t*)sstr.strs[1], strlen(sstr.strs[1]), 0xFFFF);
+
+	sprintf(send, "\n");
+	HAL_UART_Transmit(&huart2, (uint8_t*)send, strlen(send), 0xFFFF);
+
+	*/
+
+
+	if(strcmp(sstr.strs[0], "Servo") == 0 && sstr.cnt == 2)
+	{
+		int servo = atoi(sstr.strs[1]);
+
+		//char send[100];
+		sprintf(send, "Servo = %i",servo);
+
+		HAL_UART_Transmit(&huart2, (uint8_t*)send, strlen(send), 0xFFFF);
+
+	}
 }
 
 void comm_do()
@@ -26,6 +82,7 @@ void comm_do()
 		msg[index++] = buffer[0];
 
 		HAL_UART_Transmit(&huart2, (uint8_t*)msg, index, 0xFFFF);
+		parse();
 		index = 0;
 	}
 	else
@@ -38,4 +95,73 @@ void comm_do()
 		index = 0; // no mercy for long messages!!!!
 	}
 
+}
+
+void strsplit(SplittedStr_t* sstr,
+                    char* str,
+                    char splitchar,
+                    char quotechar,
+                    uint8_t maxsplits)
+{
+    uint16_t cnt = 0;
+    bool bInStr = false;
+    bool bInQuote = false;
+    char C;
+    char* pLastSplitPoint = 0;
+
+    sstr->strs[0] = str;
+    sstr->cnt = 0;
+    sstr->completelySplitted = true;
+
+    while(*str != '\0')
+    {
+        C = *str;
+
+        if (bInQuote)
+        {
+            if (C == quotechar)
+            {
+                bInQuote = false;
+            }
+        }
+        else if ( bInStr && (C == quotechar) )
+        {
+            bInQuote = true;
+        }
+        else if ( bInStr && (C == splitchar) )
+        {
+            *str = '\0';
+
+            bInStr = false;
+
+            pLastSplitPoint = str;
+        }
+        else if ( !bInStr && (C != splitchar) )
+        {
+            ++cnt;
+
+            bInStr = true;
+
+            if (cnt > maxsplits)
+            {
+                *pLastSplitPoint = splitchar;
+                sstr->completelySplitted = false;
+                sstr->cnt = maxsplits;
+                return;
+            }
+
+            sstr->strs[cnt - 1] = str;
+
+            if (C == quotechar)
+            {
+                bInQuote = true;
+            }
+        }
+
+        ++str;
+    }
+
+    sstr->cnt = cnt;
+
+    return;
 }
